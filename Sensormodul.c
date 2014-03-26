65a63efe47e5a9cf5458ad3fade04455704ea827
Ema <emabe844@student.liu.se>
2014-03-26 11:54:28 +0100

    Sensor: Klistrat in AVR-lab

--- Sensormodul.c
+++ Sensormodul.c
@@ -1,1 +1,36 @@
 
+//Börjar med att klistra in från AVR-labben
+int main(void)
+{
+    MCUCR = 0b00000011;
+    GICR = 0b01000000;
+    ADMUX = 0;
+    DDRB = 0xFF;
+    ADCSRA = 0b10001011;
+
+    sei();
+    while(1) {}
+
+    //Pseudokod typ fast inte
+    /*Start
+    Setup
+    while (styr vill ha data)
+    {
+    if RFID
+	minns rfid
+    adomvandla sensorvärden
+    omvandla till rätt enhet
+    skicka till styr
+    }*/
+
+}
+
+ISR(INT0_vect) //knapp
+{
+    ADCSRA = 0b11001011;
+}
+
+ISR(ADC_vect)
+{
+    PORTB = ADC >> 2;
+}
