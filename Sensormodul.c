672b585ed0a955b9e924661e9cfde0219c757e4c
Ema <emabe844@student.liu.se>
2014-03-27 11:30:16 +0100

    Sensor: Test

--- Sensormodul.c
+++ Sensormodul.c
@@ -1,3 +1,5 @@
+#include <avr/io.h>
+#include <avr/interupt.h>
 
 //Börjar med att klistra in från AVR-labben
 int main(void)
@@ -5,23 +7,79 @@ int main(void)
     MCUCR = 0b00000011;
     GICR = 0b01000000;
     ADMUX = 0;
-    DDRB = 0xFF;
+    DDRA = 0x00;
+    //DDRC = 0x06;
+    DDRD = 0xFF;
     ADCSRA = 0b10001011;
+
+    char i = 0; //Vilken sensor jag använder
+
+    char dFront;
+    char dRight_Front;
+    char dRight_Back;
+    char dLeft_Front;
+    char dLeft_Back;
+    char dDist;
+    char dGyro;
+
 
     sei();
     while(1) {}
 
-    //Pseudokod typ fast inte
-    /*Start
-    Setup
-    while (styr vill ha data)
+    if(/*Master säger att jag ska skicka*/)
     {
-    if RFID
-	minns rfid
-    adomvandla sensorvärden
-    omvandla till rätt enhet
-    skicka till styr
-    }*/
+        //ADMUX = i;
+        //ADCSRA = 0b11001011;
+    }
+}
+
+ISR(INT0_vect) //knapp
+{
+    ADMUX = 0;
+    ADCSRA = 0b11001011;
+}
+
+ISR(ADC_vect)
+{
+
+    i = i + 1;
+    if(i == 1)
+    {
+        dFront = ADC >> 2;
+    }
+    if(i == 2)
+    {
+        dRifht_Front = ADC >> 2;
+    }
+    if(i == 3)
+    {
+        dRifht_Back = ADC >> 2;
+    }
+    if(i == 4)
+    {
+        dLeft_Front = ADC >> 2;
+    }
+    if(i == 5)
+    {
+        dLeft_Back = ADC >> 2;
+    }
+    if(i == 6)
+        dDist = ADC >> 2;
+    }
+    if(i == 7)
+    {
+        dGyro = ADC >> 2;
+    }
+    if(i== 7)
+    {
+        i = 0;
+        ADCSRA = 0b10001011;
+    }
+    else
+    {
+        ADMUX = i;
+        ADCSRA = 0b11001011;
+    }
 
 }
 
@@ -97,12 +155,4 @@ void convert_gyro()
 
 
 
-ISR(INT0_vect) //knapp
-{
-    ADCSRA = 0b11001011;
-}
 
-ISR(ADC_vect)
-{
-    PORTB = ADC >> 2;
-}
