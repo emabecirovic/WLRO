4feda3a04e7c05d42461c101dec831d7e7f23b37
emabecirovic <emabecirovic@users.noreply.github.com>
2014-03-27 14:23:09 +0100

    Sensor: Test2

--- Sensormodul.c
+++ Sensormodul.c
@@ -1,42 +1,90 @@
 #include <avr/io.h>
-#include <avr/interupt.h>
+#include <avr/interrupt.h>
 
-//Börjar med att klistra in från AVR-labben
+ char i = 0; //Vilken sensor jag anvÃ¤nder
+
+	 char dFront;
+	 char dRight_Front;
+	 char dRight_Back;
+	 char dLeft_Front;
+	 char dLeft_Back;
+	 char dDist;
+	 char dGyro;
+
+
 int main(void)
 {
-    MCUCR = 0b00000011;
-    GICR = 0b01000000;
-    ADMUX = 0;
-    DDRA = 0x00;
-    //DDRC = 0x06;
-    DDRD = 0xFF;
-    ADCSRA = 0b10001011;
+	 //MCUCR = 0b00000011; //atmega16
+	 //GICR = 0b01000000; //atmega16
+	 
+	 MCUCR = 0b00000000;
+	 EIMSK = 0b00000001;
+	 EICRA = 0b00000011;
+	 
+	 ADMUX = 0;
+	 DDRA = 0x00;
+	 //DDRC = 0x06;
+	 //DDRD = 0x00;
+	 ADCSRA = 0b10001011;
 
-    char i = 0; //Vilken sensor jag använder
-
-    char dFront;
-    char dRight_Front;
-    char dRight_Back;
-    char dLeft_Front;
-    char dLeft_Back;
-    char dDist;
-    char dGyro;
+	
 
 
-    sei();
-    while(1) {}
-
-    if(/*Master säger att jag ska skicka*/)
+	 
+	 sei();
+	 
+    while(1)
     {
-        //ADMUX = i;
-        //ADCSRA = 0b11001011;
+        //TODO:: Please write your application code 
     }
 }
 
 ISR(INT0_vect) //knapp
 {
-    ADMUX = 0;
-    ADCSRA = 0b11001011;
+	ADMUX = 0;
+	ADCSRA = 0b11001011;
+}
+
+ISR(ADC_vect)
+{
+
+	i = i + 1;
+	if(i == 1)
+	{
+		dFront = ADC >> 2;
+	}
+	if(i == 2)
+	{
+		dRight_Front = ADC >> 2;
+	}
+	if(i == 3)
+	{
+		dRight_Back = ADC >> 2;
+	}
+	if(i == 4)
+	{
+		dLeft_Front = ADC >> 2;
+	}
+	if(i == 5)
+	{
+		dLeft_Back = ADC >> 2;
+	}
+	if(i == 6)
+	{
+		dDist = ADC >> 2;
+	}
+	if(i == 7)
+	{
+		dGyro = ADC >> 2;
+		i = 0;
+		ADCSRA = 0b10001011;
+	}
+	else
+	{
+		ADMUX = i;
+		ADCSRA = 0b11001011;
+	}
+
 }
 
 ISR(ADC_vect)
@@ -85,42 +133,42 @@ ISR(ADC_vect)
 
 void send_front()
 {
-    //skicka först 1;
+    //skicka fï¿½rst 1;
 }
 
 void send_fight_front()
 {
-    //skicka först 2;
+    //skicka fï¿½rst 2;
 }
 
 void send_right_back()
 {
-    //skicka först 3;
+    //skicka fï¿½rst 3;
 }
 
 void send_left_front()
 {
-    //skicka först 4;
+    //skicka fï¿½rst 4;
 }
 
 void send_left_back()
 {
-    //skicka först 5;
+    //skicka fï¿½rst 5;
 }
 
 void send_dist()
 {
-    //skicka först 6;
+    //skicka fï¿½rst 6;
 }
 
 void send_gyro()
 {
-    //skicka först 7;
+    //skicka fï¿½rst 7;
 }
 
 void send_RFID()
 {
-    //skicka först 8;
+    //skicka fï¿½rst 8;
 }
 
 void convert_front()
