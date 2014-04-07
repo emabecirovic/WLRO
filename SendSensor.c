e6aa9ad0152c93056b52fd3099dec35fcc099fca
robertsvensk <robertsvensk91@gmail.com>
2014-04-07 09:26:40 +0200

    Create SendSensor.c
    Started writing functions for communication between Styr & Sensor

---
+++ SendSensor.c
@@ -0,0 +1,28 @@
+/*
+ * SendSensor.c
+ *
+ * Created: 4/7/2014 8:44:12 AM
+ *  Author: robsv107
+ */ 
+
+
+#include <avr/io.h>
+
+// Master
+
+void TransmitSensor(char invalue)
+{
+  PORTB &= 0b11101111; // ss2 low
+  
+
+
+if(reglerarhöger)
+{
+  TransmitSensor(höger);
+
+
+
+
+void send()
+{
+  
