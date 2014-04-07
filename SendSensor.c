d6fc9ddc2945a2cc3286e2ea330501ed2427aa70
robertsvensk <robertsvensk91@gmail.com>
2014-04-07 11:37:20 +0200

    Update SendSensor.c

--- SendSensor.c
+++ SendSensor.c
@@ -8,7 +8,10 @@
 
 #include <avr/io.h>
 
-// Master
+/***********************MASTER*****************************/
+
+typedef int bool;
+enum {false, true};
 
 //Start bytes for transmition
 char front = 0b00000001;
@@ -16,13 +19,19 @@ char rightfront = 0b00000010;
 char rightback = 0b00000011;
 char leftfront = 0b00000100;
 char leftback = 0b00000101;
-char traveleddist = 0b00000110;
+char traveldist = 0b00000110;
 char gyro = 0b00000111;
 char RFID = 0b00001000;
 char direction = 0b00001001;
 char rightspeed = 0b00001010; 
 char leftspeed = 0b00001011;
 char stop = 0x00; //Stopbyte
+
+//Control characters
+bool regulateright = false;
+bool regulateleft = false;
+bool regulateturn = false;
+
 
 void TransmitSensor(char invalue)
 {
@@ -33,16 +42,16 @@ void TransmitSensor(char invalue)
   MasterTransmit(front); // Request front sensor
   SensorRFID = SPDR; 
   
-  if(invalue == hoger)
+  if(invalue == right)
   {
     MasterTransmit(rightfront);
-    SensorFront = SPDR; // Change so that value is stored in array or whatever Patrik want to use
+    SensorFront = SPDR; // Change so that value is stored in array or whatever Patrik wants to use
     MasterTransmit(rightback);
     SensorRightFront = SPDR;
     MasterTransmit(stop);
     SensorRightBack = SPDR;
   }
-  else if(invalue == vanster)
+  else if(invalue == left)
   {
     MasterTransmit(leftfront);
     SensorFront = SPDR;
@@ -51,7 +60,7 @@ void TransmitSensor(char invalue)
     MasterTransmit(stop);
     SensorLeftBack = SPDR;
   }
-  else if(invalue == svanga)
+  else if(invalue == turn)
   {
     MasterTransmit(gyro)
     SensorFront = SPDR;
@@ -66,15 +75,84 @@ void TransmitSensor(char invalue)
   PORTB ^= 0b00010000; // ss2 high
 }
 
+
+
 int main(void)
 {
-  if(reglerarhoger)
-    TransmitSensor(hoger);
-  else if(reglervanster)
-    TransmitSensor(vanster);
-  else if(reglersvanga)
-    TransmitSensor(svanga);
+  if(regulateright)
+    TransmitSensor(right);
+  else if(regulateleft)
+    TransmitSensor(left);
+  else if(regulateturn)
+    TransmitSensor(turn);
   else
     TransmitSensor(0x00);
+}
+
+
+/***********************SLAVE*****************************/
+
+//Start bytes for transmition
+char front = 0b00000001;
+char rightfront = 0b00000010;
+char rightback = 0b00000011;
+char leftfront = 0b00000100;
+char leftback = 0b00000101;
+char traveldist = 0b00000110;
+char gyro = 0b00000111;
+char RFID = 0b00001000;
+char stop = 0x00; //Stopbyte
+
+Void Recieve
+
+
+
+
+int main(void)
+{
+  
+  while(0<<PORTB4) // slave selected
+  {
+    selection = SlaveRecieve();
+    
+    if(selection == front)
+    {
+      SPDR = sortedValues[0];
+    }
+    else if (selection == rightfront)
+    {
+      SPDR = sortedValues[1];
+    }
+    else if (selection == rightback)
+    {
+      SPDR = sortedValues[2];
+    }
+    else if (selection == leftfront)
+    {
+      SPDR = sortedValues[3];
+    }
+    else if (selection == leftback)
+    {
+      SPDR = sortedValues[4];
+    }
+    else if (selection == traveldist)
+    {
+      SPDR = Distance;
+      Distance = 0;
+    }
+    else if (selection == gyro)
+    {
+      SPDR = sendGyro;
+      sendGyro = 0;
+    }
+    else if (selection == RFID)
+    {
+      SPDR = isRFID;
+    }
+    else if (selection == stop)
+    {
+      // behöver förmodligen inte göra något här
+    }
+  }
 }
 
