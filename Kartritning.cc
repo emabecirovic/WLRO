5bba04f13d11195d157de83cc8bd28ce2a0834c2
ekbrant <marek588@student.liu.se>
2014-04-17 10:30:30 +0200

    Update Kartritning.cc

--- Kartritning.cc
+++ Kartritning.cc
@@ -1,23 +1,28 @@
 /*
+*Kartritning.cc
+*
+*marek588
+*
+*2014-04-17
 *
 *
-*
-*
-*
-*
-*
-********************* 29 x 15 - matris **********************/
+********************* 29 x 15 - matris med nollor**********************/
 
 #include <iostream>
 
 
 int main()
 
-int map[29][15], i, j;
+{int map[29][15], i, j;
 
 for (i = 0; i < 29; i++)
   
   {for (j = 0; j < 15; j++)
   
     {map[i][j] = 0;
-     cout << 
+     cout << map[i][j];
+    }
+    
+  }
+  
+}
