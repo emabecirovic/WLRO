ac0c74487a161be008b16decbe9bb22efe2fc4ef
ekbrant <marek588@student.liu.se>
2014-04-29 10:24:54 +0200

    Update Kartritning.c

--- Kartritning.c
+++ Kartritning.c
@@ -13,20 +13,20 @@
 
 int main ()
 
-{int map[29][15], i, j;
+{int array[29][15], i, j;
 
   for (i = 0; i < 29; i++)
   
   {for (j = 0; j < 15; j++)
   
-    {map[i][j] = 0;
+    {array[i][j] = 0;
     
-    map[myposX][myposY] = //vad sensor säger att det är för ngt
+    array[myposX][myposY] = //vad sensor säger att det är för ngt
    
     
-    printf("map[%d][%d] = %d \n", i, j, map[i][j]);
+    printf("array[%d][%d] = %d \n", i, j, array[i][j]);
     
-    //alt: printf("%d ", map[i][j]); } printf("\n");
+    //alt: printf("%d ", array[i][j]); } printf("\n");
     }
     
   }
