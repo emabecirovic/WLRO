23dc1284211848fbe51dde4ddd09c13f5bbc4b77
robertsvensk <robertsvensk91@gmail.com>
2014-04-29 10:53:44 +0200

    Startup

---
+++ Search.c
@@ -0,0 +1,35 @@
+/*
+ * Search.c
+ *
+ * Created: 4/29/2014 9:26 AM
+ *  Author: robsv107
+ */
+ 
+ 
+ typedef int bool;
+ enum{false, true};
+ 
+ start.x = 15;
+ start.y = 0;
+ 
+
+int[][] createMatrix()
+{
+  int karta[29][15], i, j;
+
+  for (i = 0; i < 29; i++)
+    {
+      for (j = 0; j < 15; j++)
+      {
+        karta[i][j] = 0;
+      }
+    }
+    
+    return karta;
+}
+
+
+int main()
+{
+  
+}
