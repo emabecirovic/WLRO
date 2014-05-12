6c5f39e8f7c33601d671293c969a97923a1818e7
ekbrant <marek588@student.liu.se>
2014-05-12 16:44:37 +0200

    Update Kartritning.cc

--- Kartritning.cc
+++ Kartritning.cc
@@ -8,7 +8,7 @@
 */
 
 
-//Rita ut ett tomt (vitt) fönster, pixlar eller?, i 29*15-storlek, med RenderWindow
+//Rita ut ett tomt (vitt) fönster, pixlar eller?, i 29*15-storlek, med RenderWindow. Finns redan i nån main
 
 //Rita ut roboten på kartan, i punkten (15,0) kommer den börja. Typ en fylld fyrkant eller nåt.
 sf::RectangleShape robotposition;
@@ -16,11 +16,9 @@ robotposition.setSize(sf::Vector2f(10, 10)); //fett osäker på storleken..
 robotposition.setOutlineColor(sf::Color::Blue);
 robotposition.setOutlineThickness(5); // och tjockleken.
 robotposition.setPosition(myposX, myposY);
-
 window.draw(robotposition);
 
  
-//anvönd setfillcolor för att fylla rektangel
 
 //bör rita ut väggar på rätt plats när storleken är fixad
 sf::RectangleShape wall;
@@ -28,7 +26,6 @@ wall.setSize(sf::Vector2f(10, 10));
 wall.setOutlineColor(sf::Color::Black);
 wall.setOutlineThickness(1);
 wall.setPosition(x, y); //samma x och y som i setwall 
-
 window.draw(wall);
 
 
@@ -40,8 +37,7 @@ fire.setRadius(5);
 fire.setOutlineColor(sf::Color::Red);
 fire.setOutlineThickness(5);
 fire.setPosition(,);
-
 window.draw(fire);
 }
 
-
+//anvönd setfillcolor för att fylla rektangel
