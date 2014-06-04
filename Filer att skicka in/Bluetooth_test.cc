02470373cf7b744ff3a87471212a014952de8c76
poner538 <pontus.erlesand@gmail.com>
2014-06-04 23:00:10 +0200

    klar

--- Filer att skicka in/Bluetooth_test.cc
+++ Filer att skicka in/Bluetooth_test.cc
@@ -1,9 +1,9 @@
 /*
-* FILNAMN: Bluetooth_test.cc
+* FILNAMN: main.cc
 * PROGRAMMERARE: Grupp 9 - WLRO
 * DATUM: 2014-06-04
 *
-* .cc-fil för huvudprogrammet för kommunikation mellan Roboten och PC:n
+* mainfil för det kartritande programmet och för att styra roboten autonomt
 *
 *
 */
@@ -27,28 +27,23 @@ using namespace std;
 
 
 /************ Kartritning ***********************/
-void Drawmap(sf::RenderWindow* myWindow, int room[31][17],sf::RectangleShape* robotposition)//,sf::RectangleShape* fire, sf::RectangleShape* robotposition ,sf::RectangleShape* wall)
+void Drawmap(sf::RenderWindow* myWindow, int room[31][17],sf::RectangleShape* robotposition)
 {
-
-
-//Rita ut ett tomt (vitt) fönster, pixlar eller?, i 29*15-storlek, med RenderWindow. Finns redan i nån main
-
-//Rita ut roboten på kartan, i punkten (15,0) kommer den börja. Typ en fylld fyrkant eller nåt.
     for(int i = 0; i < 31; i++)
     {
         for(int j = 0; j < 17; j++)
         {
 
-            if(room[i][j] == 2)
+            if(room[i][j] == 3)
             {
                 robotposition->setFillColor(sf::Color::Magenta);
                 robotposition->setPosition(100 + 20*i , 590 -(20 + 20*j));
                 myWindow->draw(*robotposition);
             }
-            else if(room[i][j] == 5)
+            else if(room[i][j] == 2)
             {
                 robotposition->setFillColor(sf::Color::White);
-                robotposition->setPosition(100 + 20*i, 590 -(20 + 20*j)); //samma x och y som i setwall
+                robotposition->setPosition(100 + 20*i, 590 -(20 + 20*j));
                 myWindow->draw(*robotposition);
             }
             else if(room[i][j] == 4)
@@ -60,24 +55,20 @@ void Drawmap(sf::RenderWindow* myWindow, int room[31][17],sf::RectangleShape* ro
             else if(room[i][j] == 1)
             {
                 robotposition->setFillColor(sf::Color::Black);
-                robotposition->setPosition(100 + 20*i, 590 -(20 + 20*j)); //samma x och y som i setwall
+                robotposition->setPosition(100 + 20*i, 590 -(20 + 20*j));
                 myWindow->draw(*robotposition);
             }
             else
             {
                 robotposition->setFillColor(sf::Color::Yellow);
-                robotposition->setPosition(100 + 20*i, 590 -(20 + 20*j)); //samma x och y som i setwall
+                robotposition->setPosition(100 + 20*i, 590 -(20 + 20*j));
                 myWindow->draw(*robotposition);
             }
-
         }
-
     }
-
-//bör rita ut väggar på rätt plats när storleken är fixad
 }
 
-
+/********Fjärrstyrt läge *****************/
 bool remote_controll(Bluetooth_Serial_comm* Com_Port)
 {
     bool remote = true;
@@ -105,41 +96,37 @@ bool remote_controll(Bluetooth_Serial_comm* Com_Port)
         }
         else if(sf::Keyboard::isKeyPressed(sf::Keyboard::E))
         {
-
             Com_Port->Send_to_Bt(5);
-
 
         }
         else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
         {
-
             Com_Port->Send_to_Bt(6);
-
         }
         else
         {
             Com_Port->Send_to_Bt(0);
         }
-
     }
     return false;
 }
-
+/*************Autonomt läge*****************/
 bool autonom(Bluetooth_Serial_comm* Com_Port)
 {
+
+
+    //Initiering av variabler
     bool remote = false;
     int test;
     int direction;
-
     float yt;
 
-    //Initiering av variabler
     sf::RenderWindow* myWindow = new sf::RenderWindow(sf::VideoMode(800,600), "Who let the robot out?");
     myWindow->setFramerateLimit(60);
 
- sf::CircleShape cirkle;
+    sf::CircleShape cirkle;
     cirkle.setRadius(6);
-cirkle.setFillColor(sf::Color::Red);
+    cirkle.setFillColor(sf::Color::Red);
 
 
     sf::Clock myClock;
@@ -166,218 +153,183 @@ cirkle.setFillColor(sf::Color::Red);
     float y4 = 0;
     float y5 = 0;
     int windows_passed = 0;
-
-    int room[31][17];
-
-
-/****************Kartritande variabler *********************/
-
-sf::RectangleShape robotposition;
-robotposition.setSize(sf::Vector2f(20, 20)); //fett osäker på storleken..
-robotposition.setOutlineColor(sf::Color::White);
-robotposition.setFillColor(sf::Color::Magenta);
-robotposition.setOutlineThickness(1); // och tjockleken.
-
-int myposX = 15;
-int myposY = 1;
-int myposXtemp = 0;
-int myposYtemp = 0;
-int x2;
-int y;
-int type = 2;
-
-bool isRFID = false;
+    int delayi = 0;
+    int rfidx = 42;
+    int rfidy = 42;
+    int room[31][17]; //Kartan
 
 
-/***********************************************************/
+    /****************Kartritande variabler *********************/
+
+    sf::RectangleShape robotposition;
+    robotposition.setSize(sf::Vector2f(20, 20));
+    robotposition.setOutlineColor(sf::Color::White);
+    robotposition.setFillColor(sf::Color::Magenta);
+    robotposition.setOutlineThickness(1);
+
+    int myposX = 15;
+    int myposY = 1;
+    int myposXtemp = myposX;
+    int myposYtemp = myposY;
+    int x2;
+    int y;
+    int type = 2;
+
+    bool isRFID = true;
 
 
-   while(myWindow->isOpen())
+    /***********************************************************/
+
+
+    while(myWindow->isOpen())
     {
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::K))
         {
             myWindow->close();
         }
+        //Resetknapp
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::R))
         {
             for(int i = 0; i < 31; i++)
             {
                 for(int j = 0; j < 17; j++)
-                    {
+                {
                     room[i][j] = 0;
-                    }
+                }
             }
             myposX = 15;
             myposY = 1;
+            rfidx = 42;
+            rfidy = 42;
+            isRFID = true;
         }
+/************************ Hämta värden från Serieporten ***************/
+        /*Här hämtas värden från roboten beroende vilken label de har*/
 
         test = Com_Port->Read_from_BT();
-
-         if(test == 1)
+        //Vänster
+        if(test == 1)
+        {
+            test = Com_Port->Read_from_BT();
+            yt= test;
+            if(yt != 0)
             {
-                test = Com_Port->Read_from_BT();
-                yt= test;
-                if(yt != 0)
-                {
 
-                    y1 = ((1/yt) - 0.001086689563586)/0.000191822821525;
+                y1 = ((1/yt) - 0.001086689563586)/0.000191822821525;
+
+            }
+        }
+        //Höger fram
+        else if(test == 2)
+        {
+            yt = Com_Port->Read_from_BT();
+            if(yt != 0)
+            {
+                y2 = ((1/yt) - 0.000741938763948)/0.001637008132828;
+
+            }
+
+        }
+        //Höger bak
+        else if(test == 3)
+        {
+            yt = Com_Port->Read_from_BT();
+            if(yt != 0)
+            {
+                y3 = ((1/yt) - 0.000741938763948)/0.001637008132828;
+
+            }
+        }
+        //Fram
+        else if(test == 4)
+        {
+            yt = Com_Port->Read_from_BT();
+            if(yt != 0)
+            {
+                y4 = ((1/yt) - 0.000741938763948)/0.001637008132828;
+
+            }
+        }
+        //ledig ruta för att skicka värden som roboten kan testa , här låg förut vänster sensor bak
+        else if(test == 5)
+        {
+            yt = Com_Port->Read_from_BT();
+            if(yt != 0)
+            {
+                y5 = ((1/yt) - 0.000741938763948)/0.001637008132828;
+            }
+        }
+        //Färdad distans
+        else if(test == 6)
+        {
+            test = Com_Port->Read_from_BT();
+            //   cout << "distance: "<< test << " ";
+        }
+        //Gyrovärde
+        else if(test == 7)
+        {
+            test = Com_Port->Read_from_BT();
+        }
+        //Rfid detekterad för tester
+        else if(test == 8)
+        {
+            test = Com_Port->Read_from_BT();
+
+        }
+        //Rikting roboten färdas på kartan
+        else if(test == 9)
+        {
+            test = Com_Port->Read_from_BT();
+            direction = test;
+        }
+        //typ av ruta som roboten befinner sig i (vägg, vanliug eller RFID) för tester
+        else if(test == 10)
+        {
+            test = Com_Port->Read_from_BT();
+
+        }
+        //styrvärde för tester
+        else if(test == 11)
+        {
+            test = Com_Port->Read_from_BT();
+        }
+        // X position för roboten
+        else if(test == 12)
+        {
+            test = Com_Port->Read_from_BT();
+            myposX = test;
+        }
+        //Y position för roboten
+        else if(test == 13)
+        {
+            test = Com_Port->Read_from_BT();
+            myposY = test;
+        }
+        //Rfid's X och Y position
+        else if(test == 14)
+        {
+            rfidx = Com_Port->Read_from_BT();
+        }
+        else if(test == 15)
+        {
+            rfidy = Com_Port->Read_from_BT();
+
+        }
+        //Hela kartan skickas från roboten
+        else if (test == 16)
+        {
+            for(int i = 0; i < 31; i++)
+            {
+                for(int j = 0; j < 17; j++)
+                {
+                    test = Com_Port->Read_from_BT();
+                    room[i][j] = test;
 
                 }
-             //   cout << "Fram: "<< y1 << " ";
             }
-            else if(test == 2)
-            {
-                yt = Com_Port->Read_from_BT();
-                if(yt != 0)
-                {
-                    y2 = ((1/yt) - 0.000741938763948)/0.001637008132828;
+        }
+/*******************************************************************/
 
-                }
-              //  cout << "Höger Fram: "<< y2 << " ";
-
-            }
-            else if(test == 3)
-            {
-                yt = Com_Port->Read_from_BT();
-                if(yt != 0)
-                {
-                    y3 = ((1/yt) - 0.000741938763948)/0.001637008132828;
-
-                }
-               // cout << "Höger Bak: "<< y3 << "\n";
-
-            }
-            else if(test == 4)
-            {
-                yt = Com_Port->Read_from_BT();
-                if(yt != 0)
-                {
-                    y4 = ((1/yt) - 0.000741938763948)/0.001637008132828;
-
-                }
-               //     cout << "Vänster Fram: "<< y4 << "\n";
-            }
-
-            else if(test == 5)
-            {
-                yt = Com_Port->Read_from_BT();
-                if(yt != 0)
-                {
-                    y5 = ((1/yt) - 0.000741938763948)/0.001637008132828;
-
-                }
-                // cout << "Vänster Bak: "<< test << "\n";
-            }
-            else if(test == 6)
-            {
-                test = Com_Port->Read_from_BT();
-              //   cout << "distance: "<< test << " ";
-            }
-            else if(test == 7)
-            {
-                test = Com_Port->Read_from_BT();
-               // cout << "forstzeroY: "<< test << " ";
-            }
-            else if(test == 8)
-            {
-                test = Com_Port->Read_from_BT();
-                if(test == 1)
-                {
-                 //   cout << "hej2\n";
-                    isRFID = true;
-                }
-                else
-                {
-
-                    isRFID = false;
-                }
-
-             //   cout << "RFID: "<< test << " ";
-
-
-            }
-            else if(test == 9)
-            {
-                test = Com_Port->Read_from_BT();
-                direction = test;
-               //  cout << "direction: "<< test << " ";
-            }
-            else if(test == 10)
-            {
-                test = Com_Port->Read_from_BT();
-             //    cout << "type "<< test << " ";
-                // type = test;
-
-            }
-            else if(test == 11)
-            {
-                test = Com_Port->Read_from_BT();
-               //  cout << "styrvärde V: "<< test << "\n";
-            }
-
-            else if(test == 12)
-            {
-                test = Com_Port->Read_from_BT();
-                if(isRFID)
-                {
-                    type = 4;
-                }
-                else
-                {
-                type = 2;
-                }
-
-                myposX = test;
-              //    cout << "myposX: "<< test << " ";
-            }
-            else if(test == 13)
-            {
-                test = Com_Port->Read_from_BT();
-              //    cout << "myposY: "<< test << "\n";
-                  myposY = test;
-            }
-            else if(test == 14)
-            {
-                for(int i = 0; i < 31; i++)
-                {
-                    for(int j = 0; j < 17; j++)
-                    {
-                        test = Com_Port->Read_from_BT();
-                        room[i][j] = test;
-
-                    }
-                }
-                cout << "karta kommit\n";
-                // cout << "test fel värde: " << test << "\n";
-            }
-            else if(test == 15)
-            {
-                test = Com_Port->Read_from_BT();
-                if(direction == 1)
-                {
-                    room[myposX][myposY + test] = 1;
-
-                }
-                if(direction == 2)
-                {
-                    room[myposX - test][myposY] = 1;
-                }
-                if(direction == 3)
-                {
-                    room[myposX][myposY - test] = 1;
-                }
-                if(direction == 4)
-                {
-                    room[myposX + test][myposY] = 1;
-                }
-
-
-
-                cout << "rutor till Ö: " << test << "\n";
-            }
-
-
-        //Alla events
         while(myWindow->pollEvent(event))
         {
             switch(event.type)
@@ -395,15 +347,13 @@ bool isRFID = false;
             }
 
         }
-        x = round(myClock.getElapsedTime().asSeconds()*40);
-       windows_passed = floor(x/370);
 
 
-/********************************************************/
+        /***********************Rita på myWindow*********************************/
 
-myWindow->clear();
+        myWindow->clear();
 
-            myWindow->draw(bgSprite);
+        myWindow->draw(bgSprite);
         /*****************Vänster Fram********************/
         cirkle.setPosition(755,395 - y1);
         myWindow->draw(cirkle);
@@ -418,29 +368,32 @@ myWindow->clear();
         myWindow->draw(cirkle);
 
 
-
-
-/*************************************************/
-if(room[myposX][myposY] != 4)
-{
-    room[myposX][myposY] = type;
-}
-/**********************kartritning************************/
-
-
-
-   Drawmap(myWindow,room,&robotposition);//,fire,robotposition,wall);
-
-
-/*************************************************************/
-        myWindow->display();
-        if(room[myposX][myposY] == 2)
+        /**********************kartritning************************/
+        if(isRFID == true)
         {
-            room[myposX][myposY] = 5;
+            if((rfidx != 42) and (rfidy != 42))
+            {
+                room[rfidx][rfidy] = 4;
+                isRFID = false;
+            }
         }
-        else
-        {
 
+        if((room[myposX][myposY] != 4) and (room[myposX][myposY] != 1))
+        {
+            room[myposX][myposY] = 3;
+        }
+
+
+        Drawmap(myWindow,room,&robotposition);
+
+
+        /*************************************************************/
+        myWindow->display();
+
+
+        if(room[myposX][myposY] == 3)
+        {
+            room[myposX][myposY] = 2;
         }
 
     }
@@ -449,30 +402,26 @@ if(room[myposX][myposY] != 4)
 
     return true;
 }
-
-
-/*************tester*****************/
-
 /*********************************/
 int main()
 {
 
     Bluetooth_Serial_comm Com_Port;
     bool remote = false;
-while(1)
-{
-
-
-    if(remote == true)
+    while(1)
     {
-        remote = remote_controll(&Com_Port);
-    }
-    else
-    {
-        remote = autonom(&Com_Port);
-    }
 
-}
+
+        if(remote == true)
+        {
+            remote = remote_controll(&Com_Port);
+        }
+        else
+        {
+            remote = autonom(&Com_Port);
+        }
+
+    }
 
     Com_Port.disconnect();
 
